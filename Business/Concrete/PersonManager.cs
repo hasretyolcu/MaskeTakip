242bd8cacba4cfca1b5de62d43e49daea618f640
HASRET <HASRET@DESKTOP-PU8F4SK>
2024-02-07 17:56:18 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+      //Çıplak class kalmasın
+    public class PersonManager: IApplicantService
+    {
+        //encapsulation
+        public void ApplyForMask (Person person)
+        {
+            throw new NotImplementedException();
+        }
+        
+      
+        public List<Person> GetList()
+        {
+            throw new NotSupportedException();
+            
+        }
+
+        public bool Checkperson(Person person)
+        {
+            KPSPublicSoapClient client = new(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync
+                (new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody
+                (person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+
+        }
+    
+
+
+
+    }
+        }
+
+
+    
+   
+
